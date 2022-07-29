ab050284850fbe0bf1bde6e893bbdc4973fa233c
Mark Nyon <msnyon@hey.com>
2022-07-28 20:12:49 -0400

    Added mouseleave event to fix hamburger. Reorganized site assets

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,22 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  console.log("Leaving menu");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+function logThis(page) {
+  console.log('This happened');
+}
 
 hamburger.addEventListener("click", mobileMenu);
+navMenu.addEventListener("mouseleave", closeMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
 
 const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+
 
 const accordion = document.getElementsByClassName('container');
 
